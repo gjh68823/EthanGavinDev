b582d7726a361965a6142cbdccb1fc6df7704353
Cai Hallstrom <cthallstrom@gmail.com>
2026-06-10 01:28:09 -0500

    Fixed syntax errr in cleanJetsData

--- TIMBER/Framework/Tprime1lep/cleanjet.cc
+++ TIMBER/Framework/Tprime1lep/cleanjet.cc
@@ -429,8 +429,9 @@ RVec<RVec<float>> cleanJetsData (const float run, const bool &debug, const strin
  correction::CompoundCorrection::Ref& ak8corr,
  const RVec<TLorentzVector> &jt_p4, const RVec<float> &jt_rf, const RVec<float> &jt_murf, const RVec<float> &jt_area, const RVec<float> &jt_em, //jet_id is removed
  const RVec<TLorentzVector> &genjt_p4, const RVec<int> &jt_genidx, const RVec<TLorentzVector> &mu_p4, const RVec<int> mu_jetid, 
- const RVec<TLorentzVector> &el_p4, const RVec<int> &el_jetid, const float &rho, const float &met, const float &phi)
+ const RVec<TLorentzVector> &el_p4, const RVec<int> &el_jetid, const float &rho, const float &met, const float &phi) {
  
  RVec<int> jet_id(jt_p4.size(),9);
  return cleanJetsData(run, debug, campaign, ak4corr, ak4corrL1, ak8corr, jt_p4, jt_rf, jt_murf, jt_area, jt_em, jet_id, genjt_p4, jt_genidx, mu_p4, mu_jetidd, el_p4, el_jetid, rho, met, phi);
+ }
 //May not be fully implemented. Worth double checking.
